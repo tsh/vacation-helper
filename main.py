8341ada30846fb5001c1d10b0700c32c0288cc28
tsh <dr.tallin@gmail.com>
2022-08-20 21:05:17 +0300

    extract a's from search result

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ from selenium.webdriver.firefox.options import Options
 
 class WorldExpeditions:
     BASE_SEARCH_URL = 'https://worldexpeditions.com/advanced-search'
-    SEARCH_PARAM = 'searchKeywords'
+    SEARCH_PARAM = 'searchFromDate=2022-08-30&searchToDate=2022-08-30&searchFromDateActual=2022-08-30&searchToDateActual=2022-08-30'
 
-op = Options()
-op.add_argument("--headless")
-driver = webdriver.Firefox(executable_path='geckodriver.exe', options=op)
-driver.get(WorldExpeditions.BASE_SEARCH_URL+'?searchKeywords=Bhutan')
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.select('teaser'))
+# op = Options()
+# op.add_argument("--headless")
+# driver = webdriver.Firefox(executable_path='geckodriver.exe', options=op)
+# driver.get(WorldExpeditions.BASE_SEARCH_URL+'?searchKeywords=Bhutan')
+# html = driver.page_source
+
+with open('data/bhutan_search_result.html',) as f:
+    soup = BeautifulSoup(f, 'html.parser')
+teasers = soup.select('teaser')
+results = []
+for teaser in teasers:
+    title = teaser.find('h3').text
+    links = set()
+    for link in teaser.find_all('a', href=True):
+        links.add(link.attrs['href'])
+    results.append((title, links))
+
+
